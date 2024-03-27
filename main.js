bb4f174907e98782919fa40f8e555644bead55ee
Sérgio Calazans <88505877+Gunga-16@users.noreply.github.com>
2024-03-27 08:08:33 -0300

    text.textcontent

--- main.js
+++ main.js
@@ -1,14 +1,13 @@
-
 alert('bem vindo pessoal');
 
 let nome_usuario = '';
 
 let texto = document.querySelector('span')
 
-console.log("NOME DO USUÁRIO = " + nome_usuario);
-
     while(nome_usuario == '') {
         nome_usuario =  prompt("qual o seu nome?");
     }
+
+console.log("NOME DO USUÁRIO = " + nome_usuario);
 
 texto.textContent = nome_usuario;
