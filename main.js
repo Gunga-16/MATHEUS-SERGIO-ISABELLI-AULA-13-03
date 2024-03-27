95e2c2d97ed16309969095cc94d0ba944e413548
Sérgio Calazans <88505877+Gunga-16@users.noreply.github.com>
2024-03-27 08:04:29 -0300

    while

--- main.js
+++ main.js
@@ -1,10 +1,14 @@
 
 alert('bem vindo pessoal');
 
-let nome_usuario = prompt("qual o seu nome?");
+let nome_usuario = '';
 
 let texto = document.querySelector('span')
 
 console.log("NOME DO USUÁRIO = " + nome_usuario);
 
 texto.textContent = nome_usuario;
+
+    while(nome_usuario == '') {
+        nome_usuario =  prompt("qual o seu nome?");
+    }
