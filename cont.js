2247d48d65526867017bb8cd99bccaecce0202f6
Sérgio Calazans <88505877+Gunga-16@users.noreply.github.com>
2024-04-03 08:45:46 -0300

    a

--- cont.js
+++ cont.js
@@ -5,7 +5,7 @@ const botoes = document.querySelectorAll('.objetivos');
 
 
 
-for(let i = 0; i < botoes.lenght; i++) {
+for(let i = 0; i < botoes.length; i++) {
 console.log(i); 
     botoes[i].onclick = function(){
         botoes[i].classList.add('ativo');
