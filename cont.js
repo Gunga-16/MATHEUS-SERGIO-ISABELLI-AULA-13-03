5d1695adda74ce8fa5921697de1c326901c19ef4
Sérgio Calazans <88505877+Gunga-16@users.noreply.github.com>
2024-04-03 08:47:33 -0300

    t

--- cont.js
+++ cont.js
@@ -9,6 +9,7 @@ for(let i = 0; i < botoes.length; i++) {
 console.log(i); 
     botoes[i].onclick = function(){
         botoes[i].classList.add('ativo');
+        botoes[i-1].classList.remove('ativo');
     }
 
 //botoes[i].classList.remove('ativo');
