13ff1fe26bce81d7b19599ad037d922dbc2fb814
Sérgio Calazans <88505877+Gunga-16@users.noreply.github.com>
2024-04-22 12:00:53 -0300

    função-calculaTempo

--- cont.js
+++ cont.js
@@ -3,17 +3,16 @@ const texto = document.querySelectorAll('.aba-conteudo');
 // botoes.lenght
 
 for(let i = 0; i < botoes.length; i++) {
-console.log(i); 
-    botoes[i].onclick = function(){
-        for(let j = 0; j< botoes.length; j++){
-            botoes[j].classList.remove('ativo');
-            texto[j].classList.remove('ativa');
-        }
+    console.log(i); 
+        botoes[i].onclick = function(){
+            for(let j = 0; j< botoes.length; j++){
+                botoes[j].classList.remove('ativo');
+                texto[j].classList.remove('ativa');
+            }
         botoes[i].classList.add('ativo');
         texto[i].classList.add('ativa');
       
-    }
-
+        }
 //botoes[i].classList.remove('ativo');
 }
 
@@ -21,23 +20,27 @@ const contadores = document.querySelectorAll('.contador');
 
 const tempoObjetivo1 = new Date ('2024-11-12T00:00:00');
 const tempoObjetivo2 = new Date ('2025-01-17T00:00:00');
+const tempoObjetivo3 = new Date ('2024-12-17T00:00:00');
+const tempoObjetivo4 = new Date ('2024-12-13T00:00:00');
+
+let tempos = [tempoObjetivo1, tempoObjetivo2, tempoObjetivo3, tempoObjetivo4];
+
+function calculaTempo(tempoObjetivo){
+    let tempoAtual = new Date ();
+    let tempoFinal = tempoObjetivo - tempoAtual;
+
+    let segundos = Math.floor(tempoFinal/1000);
+    let minutos = Math.floor(segundos/60);
+    let horas = Math.floor(minutos/60);
+    let dias = Math.floor(horas/24);
+
+    segundos %= 60;
+    minutos %= 60;
+    horas %= 24;
 
 
-let tempoAtual = new Date ();
-let tempoFinal = tempoObjetivo1 - tempoAtual;
+                                                                            
 
-let segundos = Math.floor(tempoFinal/1000);
-let minutos = Math.floor(segundos/60);
-let horas = Math.floor(minutos/60);
-let dias = Math.floor(horas/24);
+    return `${dias} Dias ${horas} Horas ${minutos} Minutos ${segundos} Segundos`;
 
-segundos %= 60;
-minutos %= 60;
-horas %= 24;
-
-
-
-
-contadores[0].textContent = `${dias} Dias ${horas} Horas ${minutos} Minutos ${segundos} Segundos`;
-
-
+}
